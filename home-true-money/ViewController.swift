d230169c55b8a69f927e46afed62ef4f4ff9d94a
Hoang Thai <thai.nh@ascendcorp.com>
2019-08-13 21:52:14 +0700

    Initial Commit

---
+++ home-true-money/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  home-true-money
+//
+//  Created by peanut36k on 13/8/19.
+//  Copyright © 2019 peanut36k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
